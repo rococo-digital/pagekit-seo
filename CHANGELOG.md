f6a0ba670f893652d9dca5e2b8ac392f8efdc849
Roman <roman.lossin@icloud.com>
2018-02-13 10:50:51 +0100

    ADD: Added French language (thanks to our community / @RoyalC)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 ## 1.0.2
 
 - FIX: Fixes a bug that displayed title options, even if title overwriting is not enabled
+- ADD: Added French language (thanks to our community / @RoyalC)
 
 ## 1.0.1
 
---
+++ languages/fr_FR/messages.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'SEO Settings'               => 'Paramètres de référencement',
+    'Site Structure'             => 'Structure du site',
+    'Meta Description'           => 'Meta description',
+    'Add Meta Description'       => 'Ajouter une description méta',
+    'Use Open Graph Description' => 'Utiliser la description Open Graph',
+    'Title'                      => 'Titre',
+    'Overwrite Title'            => 'Remplacer le titre',
+    'Use Open Graph Title'       => 'Utiliser le titre Open Graph',
+    'Ordering'                   => 'Classement',
+    'Use Sitename'               => 'Utiliser le nom de site',
+    'Use Pagename'               => 'Utiliser le nom de page',
+    'Use Path'                   => 'Utiliser le chemin',
+    'Start Level'                => 'Niveau de départ',
+    'Separator'                  => 'Séparateur',
+    'Save'                       => 'Sauvegarder',
+];
